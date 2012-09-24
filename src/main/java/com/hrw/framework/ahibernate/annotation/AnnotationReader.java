2482b6d80ccde122dbfaa0d318430f0c3e904848
MadeInChina <slamhan1987@hotmail.com>
2012-09-24 09:40:38 +0800

    fix annotation test issue.

--- src/main/java/com/hrw/framework/ahibernate/annotation/AnnotationReader.java
+++ src/main/java/com/hrw/framework/ahibernate/annotation/AnnotationReader.java
@@ -2,8 +2,9 @@
 package com.hrw.framework.ahibernate.annotation;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.AnnotatedElement;
-import java.util.ArrayList;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +17,19 @@ public class AnnotationReader {
     private String propertyName;
     private transient Map<Class, Annotation> annotationsMap;
     private transient Annotation[] annotations;
+    private PropertyType propertyType;
+    private AccessibleObject mirroredAttribute;
 
     static {
         annotationToXml = new HashMap<Class, String>();
         annotationToXml.put(Table.class, "table");
+        annotationToXml.put(Id.class, "id");
+    }
+
+    private enum PropertyType {
+        PROPERTY,
+        FIELD,
+        METHOD
     }
 
     public AnnotationReader(AnnotatedElement el) {
@@ -27,6 +37,20 @@ public class AnnotationReader {
         if (el instanceof Class) {
             Class clazz = (Class) el;
             className = clazz.getName();
+        } else if (el instanceof Field) {
+            Field field = (Field) el;
+            className = field.getDeclaringClass().getName();
+            propertyName = field.getName();
+            propertyType = PropertyType.FIELD;
+            String expectedGetter = "get" + Character.toUpperCase(propertyName.charAt(0))
+                    + propertyName.substring(
+                            1
+                            );
+            try {
+                mirroredAttribute = field.getDeclaringClass().getDeclaredMethod(expectedGetter);
+            } catch (NoSuchMethodException e) {
+                // no method
+            }
         }
     }
 
@@ -35,7 +59,7 @@ public class AnnotationReader {
         return (T) annotationsMap.get(annotationType);
     }
 
-    public Annotation[] getJavaAnnotations() {
+    private Annotation[] getJavaAnnotations() {
         return element.getAnnotations();
     }
 
@@ -60,17 +84,24 @@ public class AnnotationReader {
 
     private void initAnnotations() {
         if (annotations == null) {
-            if (className != null) {
+            // is a class
+            if (className != null && propertyName == null) {
                 Annotation[] annotations = getJavaAnnotations();
-                List<Annotation> annotationList = new ArrayList<Annotation>(annotations.length + 5);
                 annotationsMap = new HashMap<Class, Annotation>(annotations.length + 5);
                 for (Annotation annotation : annotations) {
-                    if (!annotationToXml.containsKey(annotation.annotationType())) {
-                        // unknown annotations are left over
-                        annotationList.add(annotation);
+                    if (annotationToXml.containsKey(annotation.annotationType())) {
+                        annotationsMap.put(annotation.annotationType(), annotation);
                     }
                 }
 
+            } else {
+                this.annotations = getJavaAnnotations();
+                annotationsMap = new HashMap<Class, Annotation>(annotations.length + 5);
+                for (Annotation annotation : annotations) {
+                    if (annotationToXml.containsKey(annotation.annotationType())) {
+                        annotationsMap.put(annotation.annotationType(), annotation);
+                    }
+                }
             }
         }
     }
--- src/test/java/com/hrw/framework/ahibernate/test/annotations/AnnotationReaderTest.java
+++ src/test/java/com/hrw/framework/ahibernate/test/annotations/AnnotationReaderTest.java
@@ -1,13 +1,15 @@
 
 package com.hrw.framework.ahibernate.test.annotations;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertNotNull;
+
+import java.lang.reflect.Field;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.hrw.framework.ahibernate.annotation.AnnotationReader;
+import com.hrw.framework.ahibernate.annotation.Id;
 import com.hrw.framework.ahibernate.annotation.Table;
 import com.hrw.framework.ahibernate.test.domain.Demo;
 
@@ -21,14 +23,21 @@ public class AnnotationReaderTest {
     }
 
     @Test
-    public void should_return_null_when_get_annotations() {
+    public void should_return_not_null_when_get_table_annotation() {
         mAnnotationReader = new AnnotationReader(Demo.class);
-        assertNotNull(mAnnotationReader.getJavaAnnotations());
+        assertNotNull(mAnnotationReader.getAnnotation(Table.class));
     }
 
     @Test
-    public void should_return_null_when_get_annotation() {
-        mAnnotationReader = new AnnotationReader(Demo.class);
-        assertNotNull(mAnnotationReader.getAnnotation(Table.class));
+    public void should_return_not_null_when_get_id_annotation() {
+        Field idField;
+        try {
+            idField = Demo.class.getDeclaredField("id");
+            assertNotNull(idField);
+            mAnnotationReader = new AnnotationReader(idField);
+            assertNotNull(mAnnotationReader.getAnnotation(Id.class));
+        } catch (SecurityException e) {
+        } catch (NoSuchFieldException e) {
+        }
     }
 }
